d46de040633a5a02b4841ad9a4229f4969c1c77d
GonzaloArmoa <gonzalo-armoa@hotmail.com>
2024-07-12 17:43:11 -0300

    wip: Agregando lista de proyectos en ProjectsPage done!

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,5 +1,62 @@
 export const ProjectsPage = () => {
-
+  
+  const Projects = [
+    {
+      name: "Calculadora - HTML+ CSS + JS",
+      image:"",
+      url: "/about"
+    },
+    {
+      name: "Emilia Colle - Blog HTML + CSS",
+      image:"",
+      url: "/about"
+    },
+    {
+      name: "Menu - Simple Page HTML + CSS",
+      image:"",
+      url: "/about"
+    },
+    {
+      name: "Mercado Liebre - CRUD JS",
+      image:"",
+      url: "/about"
+    },
+    {
+      name: "Sip n' Ship - E-commerce JS + REACT",
+      image:"",
+      url: "/about"
+    },
+    {
+      name: "Pimienta y Sal - Landing Page- HTML + CSS + JS",
+      image:"",
+      url: "/about"
+    },
+    {
+      name: "Craftsy - E-commerce HTML + CSS + JS",
+      image:"",
+      url: "/about"
+    },
+    {
+      name: "Veterinaria - Crud JS + REACT + HOOKS",
+      image:"",
+      url: "/about"
+    },
+    {
+      name: "Clima APP - APIS + REACT",
+      image:"",
+      url: "/about"
+    },
+    {
+      name: "Project Manager - APIS + Crud JS + HOOKS + REACT",
+      image:"",
+      url: "/about"
+    },
+    {
+    name: "Kitchening - BOOTSTRAP JS + HTML + CSS",
+    image:"",
+    url: "/about"
+  }
+  ];
   return (
     <>
     ProjectsPage
