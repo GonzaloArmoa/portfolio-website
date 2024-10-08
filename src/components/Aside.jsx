150bd1b76830c5e2cf98a271f45d3d373e2cc359
Gonzalo Armoa <garmoa.dev@gmail.com>
2024-10-08 18:16:57 -0300

    Se agrega Imagen de Perfil para ajustes de diseño

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,8 +1,12 @@
+import UserImage from "../assets/defaultUser.png";
+
 export const Aside = () => {
   return (
     <>
-      <div className=" h-64 mt-48 border-double border-4 border-gray-600 rounded-md bg-orange-50 py-6">
-        <div className="-mt-32 mb-5 border-double border-4  border-gray-600 rounded-full bg-orange-50 mx-auto lg:w-56 lg:h-56"></div>
+      <div className=" h-64 mt-48 border-double border-4 border-gray-600 rounded-md bg-orange-50">
+        <div className="user-image -mt-32 mb-5 border-double border-4  border-gray-600 rounded-full bg-orange-50 mx-auto lg:w-56 lg:h-56">
+        <img className="w-full h-full" src={UserImage} alt="Foto de Perfil" />
+        </div>
         <div>
           <h1 className="text-center text-2xl font-bold">Gonzalo Armoa</h1>
           <h3 className="text-center">Web Developer</h3>
